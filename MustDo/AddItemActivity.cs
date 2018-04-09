bf1e4d407b3cbb5f1c40634a8da458906e6588b1
Johannes Gansowen <jhansgansowen@gmail.com>
2018-04-09 11:42:55 -0400

    Add controls(EditTex and Button) to AddItemActivity

--- MustDo/AddItemActivity.cs
+++ MustDo/AddItemActivity.cs
@@ -7,6 +7,7 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 
@@ -20,6 +21,13 @@ namespace MustDo
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.activity_add_item);
+
+            var buttonAdd = FindViewById<Button>(Resource.Id.buttonAddTask);
+            var editTextTaskName = FindViewById<EditText>(Resource.Id.editTextTaskName);
+
+            buttonAdd.Click += delegate {
+                Log.Debug("***** DEBUG *****", "Textfield contains " + editTextTaskName.Text);
+            };
         }
     }
 }
